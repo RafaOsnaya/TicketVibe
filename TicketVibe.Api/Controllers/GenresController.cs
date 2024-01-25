09aaaeaa63ba15d32e26deea3e706e91cefad003
Rafa Osnaya <osnayadeluciorafael@hotmail.com>
2024-01-25 01:12:53 -0600

    Gender entity is created with its repository and controller

---
+++ TicketVibe.Api/Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketVibe.Entities;
+using TicketVibe.Repositories;
+
+namespace TicketVibe.Api.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
+    {
+        private readonly GenreRepository genreRepository;
+
+        public GenresController(GenreRepository genreRepository)
+        {
+            this.genreRepository = genreRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Genre>> GetAllGenres()
+        {
+            return Ok(genreRepository.GetAllGenres());
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Genre?> GetGenreById(int id)
+        {
+            var genre = genreRepository.GetGenreById(id);
+            if (genre is null)
+            {
+                return NotFound();
+            }
+            return Ok(genre);
+        }
+
+
+    }
+}
---
+++ TicketVibe.Repositories/GenreRepository.cs
@@ -0,0 +1,63 @@
+﻿using TicketVibe.Entities;
+namespace TicketVibe.Repositories
+
+{
+    public class GenreRepository
+    {
+        private readonly List<Genre> genreList = new List<Genre>();
+
+        public GenreRepository()
+        {
+            genreList.Add(new Genre { Id = 1, Name = "Hip Hop"});
+            genreList.Add(new Genre { Id = 2, Name = "R&B"});
+            genreList.Add(new Genre { Id = 3, Name = "Salsa" });
+            genreList.Add(new Genre { Id = 4, Name = "Cumbia"});
+            genreList.Add(new Genre { Id = 5, Name = "Rock" });
+        }
+
+        //Metodos
+        public List<Genre> GetAllGenres()
+        {
+            return genreList;
+        }
+
+        public Genre? GetGenreById(int id)
+        {
+            return genreList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void AddGenre(Genre genre)
+        {
+            genre.Id = genreList.Max(x => x.Id) + 1;
+            genreList.Add(genre);
+        }
+
+        public void UpdateGenre(int id,  Genre genre)
+        {
+            var item = GetGenreById(id);
+            if(item is not null)
+            {
+                item.Name = genre.Name;
+                item.Status = genre.Status;
+            }
+            else
+            {
+                throw new Exception("Genre not found");
+            }
+        }
+
+        public void DeleteGenre(int id)
+        {
+            var item = GetGenreById(id);
+            if (item is not null)
+            {
+                genreList.Remove(item);
+            }
+            else
+            {
+                throw new Exception("Genre not found");
+            }
+        }
+
+    }
+}
