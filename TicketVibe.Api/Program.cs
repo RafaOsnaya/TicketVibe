d3053e52877187c6344370102947af8e9ea07900
Rafa Osnaya <osnayadeluciorafael@hotmail.com>
2024-02-07 00:49:38 -0600

    configuring persistence layer and EF

--- TicketVibe.Api/Program.cs
+++ TicketVibe.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TicketVibe.Entities;
+using TicketVibe.Persistence;
 using TicketVibe.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configuring context
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 
 //Registering  services
 builder.Services.AddSingleton<GenreRepository>();
@@ -25,6 +36,18 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGet("api/holaMundo", () => "Hello World!");
+app.MapGet("api/genres-minimal", (GenreRepository genreRepository) =>
+{
+    return genreRepository.GetAllGenres();
+});
+
+app.MapPost("api/genres-minimal", (Genre genre, GenreRepository genreRepository) =>
+{
+    genreRepository.AddGenre(genre);
+    return Results.Ok(genre);
+});
 
 app.MapControllers();
 
---
+++ TicketVibe.Persistence/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketVibe.Entities;
+
+namespace TicketVibe.Persistence
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<Genre>().Property(g => g.Name).IsRequired().HasMaxLength(50);        
+
+        }
+
+        //Entities To Tables
+        public DbSet<Genre> Genres { get; set; }
+
+
+    }
+}
